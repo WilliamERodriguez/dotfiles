d1aa506bbcd9987966e18177bb95ab2694d188f6
William E Rodriguez <WilliamE.Rodriguez@Proton.me>
2026-01-26 11:11:33 -0500

    Updated nvim config file

--- init.lua
+++ init.lua
@@ -1,10 +1,15 @@
 -- Global Settings
-vim.opt.number = true				-- Show line numbers (essential for debugging)
-vim.opt.relativenumber = true		-- Relative numbers help you jump lines faster
-vim.opt.mouse = 'a'					-- Enable mouse support (Good for beginners)
-vim.opt.ignorecase = true			-- Ignore case in search patterns
-vim.opt.smartcase = true			-- ...unless search has a capital letter
-vim.opt.shiftwidth = 4				-- Size of an indent
-vim.opt.tabstop = 4					-- Number of spaces tabs count for
-vim.opt.termguicolors = true		-- Enable 24-bit RGB color
-vim.cmd('colorscheme habamax')		-- Preinstalled color scheme for neovim
+vim.opt.number = true           -- Show line numbers
+vim.opt.relativenumber = true   -- Relative numbers for faster jumping
+vim.opt.mouse = 'a'             -- Enable mouse support
+vim.opt.ignorecase = true       -- Ignore case in search
+vim.opt.smartcase = true        -- ...unless search has a capital
+vim.opt.shiftwidth = 4          -- Size of an indent
+vim.opt.tabstop = 4             -- Number of spaces tabs count for
+vim.opt.termguicolors = true    -- Enable 24-bit RGB color
+
+-- Theme
+vim.cmd('colorscheme habamax')
+
+-- Integrity Check
+print("Config Loaded Successfully!")
