b5e791df874204354491a51c5a22dc622a94d7ca
William E Rodriguez <WilliamE.Rodriguez@Proton.me>
2026-01-26 10:29:56 -0500

    Basic preferences for nvim config

---
+++ init.lua
@@ -0,0 +1,9 @@
+-- Global Settings
+vim.opt.number = true			-- Show line numbers (essential for debugging)
+vim.opt.relativenumber = true		-- Relative numbers help you jump lines faster
+vim.opt.mouse = 'a'			-- Enable mouse support (Good for beginners)
+vim.opt.ignorecase = true		-- Ignore case in search patterns
+vim.opt.smartcase = true		-- ...unless search has a capital letter
+vim.opt.shiftwidth = 4			-- Size of an indent
+vim.opt.tabstop = 4			-- Number of spaces tabs count for
+vim.opt.termguicolors = true		-- Enable 24-bit RGB color
